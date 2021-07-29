64dd571327cc700c0680c28a00d8fac21cc14f3b
Golkar <Golkar@DESKTOP-SBT0IB5>
2021-07-29 22:34:03 +0430

    Add project files.

---
+++ src/MyCompanyName.AbpZeroTemplate.Web.Mvc/Areas/AppAreaName/Views/Shared/Components/AppAreaNameActiveUserDelegationsCombobox/AppAreaNameActiveUserDelegationsComboboxViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCompanyName.AbpZeroTemplate.Authorization.Delegation;
+using MyCompanyName.AbpZeroTemplate.Authorization.Users.Delegation;
+using MyCompanyName.AbpZeroTemplate.Web.Areas.AppAreaName.Models.Layout;
+using MyCompanyName.AbpZeroTemplate.Web.Views;
+
+namespace MyCompanyName.AbpZeroTemplate.Web.Areas.AppAreaName.Views.Shared.Components.AppAreaNameActiveUserDelegationsCombobox
+{
+    public class AppAreaNameActiveUserDelegationsComboboxViewComponent : AbpZeroTemplateViewComponent
+    {
+        private readonly IUserDelegationAppService _userDelegationAppService;
+        private readonly IUserDelegationConfiguration _userDelegationConfiguration;
+
+        public AppAreaNameActiveUserDelegationsComboboxViewComponent(
+            IUserDelegationAppService userDelegationAppService, 
+            IUserDelegationConfiguration userDelegationConfiguration)
+        {
+            _userDelegationAppService = userDelegationAppService;
+            _userDelegationConfiguration = userDelegationConfiguration;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string logoSkin = null, string logoClass = "")
+        {
+            var activeUserDelegations = await _userDelegationAppService.GetActiveUserDelegations();
+            var model = new ActiveUserDelegationsComboboxViewModel
+            {
+                UserDelegations = activeUserDelegations,
+                UserDelegationConfiguration = _userDelegationConfiguration
+            };
+
+            return View(model);
+        }
+    }
+}
